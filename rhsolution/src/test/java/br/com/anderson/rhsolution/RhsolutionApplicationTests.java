852f4ef191644f97f8f7becd2faf3c4af56742a2
anderson <anderson361163@gmail.com>
2024-04-17 22:22:09 -0300

    feat: Adicionando um teste inicial de Google Drive

--- rhsolution/src/test/java/br/com/anderson/rhsolution/RhsolutionApplicationTests.java
+++ rhsolution/src/test/java/br/com/anderson/rhsolution/RhsolutionApplicationTests.java
@@ -1,13 +1,23 @@
 package br.com.anderson.rhsolution;
 
+import java.nio.file.Paths;
+
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RhsolutionApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void hello() {
+		 String driverPath = Paths.get("drivers", "chromedriver").toAbsolutePath().toString();
+		
+		System.setProperty("Webdriver.chrome.driver", driverPath);
+		WebDriver browser = new ChromeDriver();
+		browser.navigate().to("http://localhost:80");
+		browser.quit();		
 	}
 
 }
